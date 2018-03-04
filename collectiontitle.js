b44d06b4ce4392c2aa697d738d7097ba44515a09
Aayush Sinha <asinha38@sapient.com>
2018-03-04 16:02:11 +0530

    Delete Element Added

--- collectiontitle.js
+++ collectiontitle.js
@@ -1,16 +1,12 @@
 var a = {};
-
+var c=0;
 function setcollectiontitle() {
-
     var title = document.getElementById('collectiontitle').value;
-
     a[title] = [];
-
-    var add = `<ul id="${title}"><a href="#!">${title}</a><button class='btn' onclick='deleteelem(${title})'>Delete</button></ul>`;
-
-    var add1 = `<ul id="${title}" onclick="addtocollection1('${title}')"><a href="#!" data-dismiss="modal">${title}</a></ul>`;
-
+    var s=`${title}${c}`;
+    console.log(s);
+    var add = `<ul id="${title}"><a href="#!">${title}</a><button class='btn' onclick="deleteelem('${title}','${s}')">Delete</button></ul>`;
+    var add1 = `<ul id="${s}" onclick="addtocollection1('${title}')"><a href="#!" data-dismiss="modal">${title}</a></ul>`;
     document.getElementById('coltitle').innerHTML += add;
-
     document.getElementById('bod').innerHTML += add1;
 };
--- deleteelement.js
+++ deleteelement.js
@@ -1,7 +1,9 @@
-function deleteelem (title) {
+function deleteelem (title,mtitle) {
 	console.log(title);
 	var c=document.getElementById(title);
+	var c1=document.getElementById(mtitle);
 	console.log(c);
 	console.log(c.parentNode);
 	c.parentNode.removeChild(c);
+	c1.parentNode.removeChild(c1);
 	}
